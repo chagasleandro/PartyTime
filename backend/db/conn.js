da1b4f48e25cd7bd6c8608e7776ca1a9587d808e
chagasleandro <69199409+chagasleandro@users.noreply.github.com>
2023-02-28 21:25:46 -0300

    add banco de dados

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -5,7 +5,7 @@ async function main() {
     mongoose.set("strictQuery", true);
 
     await mongoose.connect(
-      "mongodb+srv://matheus:bzCjl5MYrVPppveR@cluster0.tskbha0.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://leandro:672758le@cluster0.tskbha0.mongodb.net/?retryWrites=true&w=majority"
     );
 
     console.log("Conectado!");
